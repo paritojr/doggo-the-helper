736dc410298a0d6968b427f8ef787ff5dbc3c853
paritojr <141878959+paritojr@users.noreply.github.com>
2026-01-19 10:36:59 -0500

    change server stats ui

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, REST, EmbedBuilder, ActivityType } = require("discord.js");
+const { Client, GatewayIntentBits, REST, EmbedBuilder, ActivityType, time } = require("discord.js");
 const { AutoModerationRuleTriggerType, Routes } = require('discord-api-types/v10');
 const client = new Client({
     intents: [
@@ -322,22 +322,25 @@ client.on("interactionCreate", async (interaction) => {
         const guild = interaction.guild;
         await guild.channels.fetch();
         const allMembers = await guild.members.fetch();
+        const iconUrl = interaction.guild.iconURL({ dynamic: true, size: 256 });
         const memberCount = allMembers.size;
+        const creationDate = guild.createdAt;
         const botCount = allMembers.filter((member) => member.user.bot).size;
         const humanCount = allMembers.filter((member) => !member.user.bot).size;
-        const channelCount = guild.channels.cache.filter((c) =>(c.type === 0 || c.type === 2 || c.type === 5 || c.type === 15) && c.viewable).size;
+        const channelCount = guild.channels.cache.filter((c) =>(c.type === 0 || c.type === 2 || c.type === 5 || c.type === 13 || c.type === 15) && c.viewable).size;
         const roleCount = guild.roles.cache.size;
         const serverOwner = await guild.fetchOwner();
+        const finalMemberCount = `${botCount} bots and ${humanCount} humans (${memberCount} total)`
         const statsEmbed = new EmbedBuilder()
             .setTitle(`server stats for ${guild.name}`)
             .setColor("#3060f1")
+            .setThumbnail(iconUrl)
             .addFields(
-                { name: "members", value: String(memberCount), inline: true },
-                { name: "bots", value: String(botCount), inline: true },
-                { name: "humans", value: String(humanCount), inline: true },
+                { name: "created at", value: time(creationDate, 'F'), inline: false },
+                { name: "members", value: String(finalMemberCount), inline: false },
                 { name: "channels", value: String(channelCount), inline: true },
                 { name: "roles", value: String(roleCount), inline: true },
-                { name: "owner", value: `<@${serverOwner.user.id}>`, inline: true },
+                { name: "owner", value: `<@${serverOwner.user.id}>`, inline: false },
             )
             .setFooter({ text: "wowsers" })
             .setTimestamp();
