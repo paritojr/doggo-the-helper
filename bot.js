3490c6c722036ce1e1aac41417467c20d0aa692e
paritojr <141878959+paritojr@users.noreply.github.com>
2026-01-27 23:43:41 -0500

    update the bot :O

--- bot.js
+++ bot.js
@@ -350,15 +350,26 @@ client.on("interactionCreate", async (interaction) => {
         });
     } else if (commandName === "userstats") {
         const user = interaction.options.getUser("user") || interaction.user;
+        const user2 = await user.fetch(true);
         const member = await interaction.guild.members.fetch(user.id);
-        const joinedAt = member.joinedAt.toLocaleString(undefined, { dateStyle: 'short' });
-        const createdAt = user.createdAt.toLocaleString(undefined, { dateStyle: 'short' });
+        const avatarUrl = user.displayAvatarURL();
+        const accentColor = user2.hexAccentColor || "#3060f1";
+        const joinedUnix = Math.floor(member.joinedTimestamp / 1000);
+        const joinedAt = `<t:${Math.floor(member.joinedTimestamp / 1000)}:d> (<t:${joinedUnix}:R>)`;
+        const createdUnix = Math.floor(user.createdTimestamp / 1000);
+        const createdAt = `<t:${Math.floor(user.createdTimestamp / 1000)}:d> (<t:${createdUnix}:R>)`;
+        const userType = user.bot ? "bot" : "human";
+        const isBooster = member.premiumSince ? "yeah!" : "nope";
         const userEmbed = new EmbedBuilder()
             .setTitle(`user stats for ${user.tag}`)
-            .setColor("#3060f1")
+            .setColor(accentColor)
+            .setThumbnail(avatarUrl)
             .addFields(
-                { name: "joined at", value: String(joinedAt), inline: true },
-                { name: "created at", value: String(createdAt), inline: true },
+                { name: "joined at", value: joinedAt, inline: true },
+                { name: "created at", value: createdAt, inline: true },
+                { name: "type", value: `\`${userType}\``, inline: false },
+                { name: "avatar", value: `[click here idk](${avatarUrl})`, inline: false },
+                { name: "booster?", value: String(isBooster), inline: false },
             )
             .setFooter({ text: "amazing" })
             .setTimestamp();
