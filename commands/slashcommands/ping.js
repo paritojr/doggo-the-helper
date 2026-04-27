1b4586ccb6ab5047bedd6de65dad71f7a74599f6
paritojr <141878959+paritojr@users.noreply.github.com>
2026-04-27 23:03:15 +0000

    update ping command

--- commands/slashcommands/ping.js
+++ commands/slashcommands/ping.js
@@ -1,4 +1,11 @@
-function ping(interaction) {
-    return interaction.reply({content: `pong! hello ${interaction.user}!`,ephemeral: true});
+async function ping(interaction) {
+    const botLatency = Date.now() - interaction.createdTimestamp;
+    const apiLatency = Math.round(interaction.client.ws.ping);
+
+    return interaction.reply({
+        content: `pong! hello ${interaction.user}!\nlatency (bot): ${botLatency}ms\nlatency (API): ${apiLatency}ms`,
+        ephemeral: true
+    });
 }
-module.exports = { ping }
+
+module.exports = { ping };
