ccc91670653459ca8e3f40637afeedfad6903ea9
paritojr <141878959+paritojr@users.noreply.github.com>
2026-05-01 00:06:05 +0000

    update random command

--- commands/slashcommands/random.js
+++ commands/slashcommands/random.js
@@ -1,35 +1,30 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder } from 'discord.js';
 export default {
    data: new SlashCommandBuilder()
         .setName('random')
-        .setDescription('get a random dog or cat!'),
+        .setDescription("get a random cat, or a random dog")
+        .addSubcommand(subcommand =>
+         subcommand
+         .setName('cat')
+         .setDescription('get a random cat!')
+         )
+        .addSubcommand(subcommand =>
+          subcommand
+          .setName('dog')
+          .setDescription('get a random dog!')
+         ),
 
    async execute(interaction) {
-      let finalUrl;
-      let type;
-      const roll = Math.random();
-      if (roll < 0.5) {
-         const res = await fetch('https://random.dog/woof.json');
+      const subcommand = interaction.options.getSubcommand();
+      if (subcommand === "dog") {
+         const res = await fetch('https://dog.ceo/api/breeds/image/random');
          const data = await res.json();
-         finalUrl = data.url;
-         type = "dog";
-      } else {
+         interaction.reply(data.message);
+      } else if (subcommand === "cat") {
          const randomNumber = Math.floor(Math.random() * 1986);
          const res = await fetch(`https://cataas.com/api/cats?limit=1&skip=${randomNumber}`);
          const data = await res.json();
-         finalUrl = `https://cataas.com/cat/${data[0].id}`;
-         type = "cat";
+         interaction.reply(`https://cataas.com/cat/${data[0].id}`);
       }
-      const userEmbed = new EmbedBuilder()
-         .setTitle(`${interaction.user.tag}'s random ${type}!`)
-         .setColor("#3060f1")
-         .setImage(finalUrl)
-         .setFooter({
-            text: "fantastic"
-         })
-         .setTimestamp();
-      await interaction.reply({
-         embeds: [userEmbed],
-      });
    }
 };
