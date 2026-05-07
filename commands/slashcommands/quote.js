dbc90090abcba0deefe5b917b2fce6faacb56cd1
paritojr <141878959+paritojr@users.noreply.github.com>
2026-05-07 23:48:57 +0000

    add quote command

---
+++ commands/slashcommands/quote.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+export default {
+  data: new SlashCommandBuilder()
+    .setName("quote")
+    .setDescription("send a random quote"),
+
+  async execute(interaction) {
+    try {
+        const res = await fetch('https://zenquotes.io/api/random');
+        const json = await res.json();
+        const quote = json[0];
+        const embed = new EmbedBuilder()
+          .setDescription(`${quote.q} - ${quote.a}`)
+          .setColor(0x2b2d31);
+        await interaction.reply({ embeds: [embed] });
+    } catch (err) {
+        console.error(err);
+    }
+  },
+};
