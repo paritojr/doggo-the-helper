649af932730a9cc507b1b4585791f6b17c38b419
paritojr <141878959+paritojr@users.noreply.github.com>
2026-05-24 21:30:05 +0000

    idk

--- src/commands/textcommands/danger.js
+++ src/commands/textcommands/danger.js
@@ -8,10 +8,10 @@ export default {
   modOnly: true,
   async execute(message) {
     if (!message.guild) return;
-
     if (!message.member.permissions.has("ManageGuild") && !message.member.permissions.has("Administrator")) {
       return message.channel.send("you can't use this!");
     }
+
     const channel = message.mentions.channels.first();
     if (!channel) {
       return message.channel.send(`usage: \\${config.prefix}danger #channel`);
