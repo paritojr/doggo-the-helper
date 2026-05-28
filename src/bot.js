b7b76346f0682b47c6fa3bd2bdf4e73a22506514
paritojr <141878959+paritojr@users.noreply.github.com>
2026-05-28 04:02:10 +0000

    remove migration logic

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import "./migration.js";
 import { Client, GatewayIntentBits, REST, ActivityType } from "discord.js";
 import { Routes } from "discord-api-types/v10";
 import { slashcmds, textcmds } from "./commands/index.js";
