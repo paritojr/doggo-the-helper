e394f2dfdefdb1e84d98bd36f63ab6feac0558a1
paritojr <141878959+paritojr@users.noreply.github.com>
2026-07-07 04:42:57 +0000

    the ultimate rename

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 import "./textcommands.js";
 import "./slashcommands.js";
 import "./activity.js";
-import "./shyt.js";
-import "./sBoards.js"
+import "./misc.js";
+import "./sboards.js"
 import "./web.js";
 import "./updater.js";
 console.log("starting da bot...");
