e9342dab66c3c90043dbbe428b9c25e01e8cceab
paritojr <141878959+paritojr@users.noreply.github.com>
2026-07-23 01:04:12 +0000

    spacing shit 9

--- src/commands/slashcommands/quote.js
+++ src/commands/slashcommands/quote.js
@@ -8,21 +8,21 @@ export default {
 
   async execute(interaction) {
     try {
-        await interaction.deferReply();
-        const res = await fetch('https://zenquotes.io/api/random');
-        const json = await res.json();
-        const quote = json[0];
-        const embed = new EmbedBuilder()
-          .setDescription(`*"${quote.q}"*\n— ${quote.a}`)
-          .setColor(0x2b2d31);
-        await interaction.editReply({ embeds: [embed] });
+      await interaction.deferReply();
+      const res = await fetch('https://zenquotes.io/api/random');
+      const json = await res.json();
+      const quote = json[0];
+      const embed = new EmbedBuilder()
+        .setDescription(`*"${quote.q}"*\n— ${quote.a}`)
+        .setColor(0x2b2d31);
+      await interaction.editReply({ embeds: [embed] });
     } catch (err) {
-        console.error(err);
-        try {
-          await interaction.deleteReply();
-        } catch (derr) {
-          console.error("error (again wtf):", derr);
-        }
+      console.error(err);
+      try {
+        await interaction.deleteReply();
+      } catch (derr) {
+        console.error("error (again wtf):", derr);
+      }
     }
   },
 };
